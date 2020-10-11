640aceb98115028d3446ce503823421c47d3b4ff
JessicaIsri <65822756+JessicaIsri@users.noreply.github.com>
2020-10-11 20:42:36 -0300

    Add files via upload

---
+++ Valcode/src/main/java/com/fatec/Valcode/ValcodeApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.Valcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValcodeApplication.class, args);
+	}
+
+}
